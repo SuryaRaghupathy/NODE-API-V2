a691463133d71491d6e60a9d671e1bd832d34328
SuryaRaghupathy <suryaraghupathy@gmail.com>
2023-11-27 17:10:04 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+
+app.use(express.json());
 
 //routes
 
 app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
-
-app.listen(3000, () => {
-  console.log(`Node API app is running on port 3000`);
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
 });
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://Admin:admin123456@apiproject.qimphwk.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
